b98509b2e0ef68714d4122fef74b9bbc4be7a383
racha sai <hr_saidouni@esi.dz>
2020-12-20 13:56:23 +0100

    the homePage

---
+++ src/pages/homePage/homepage.component.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import './homepage.styles.scss';
+
+import DirectoryMenu from '../../components/directory-menu/directory-menu.component.jsx';
+
+const HomePage =()=>( 
+
+<div className="homepage">
+    
+    < DirectoryMenu/>
+    
+        
+   
+
+</div>
+
+
+
+) ;export default HomePage;
